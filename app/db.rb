c7626b0eba6c71323eb74c67c012e2210e5cecda
bodyyyya <vbogdanua177@gmail.com>
2024-02-01 19:46:20 +0200

    moved db ActiveRecord::Base from class Scraper

--- app/db.rb
+++ app/db.rb
@@ -13,4 +13,15 @@ class Database
   def self.connect_db
     self.connect
   end
+
+  def self.create_vacancies_table
+    ActiveRecord::Base.connection.create_table :vacancies do |t|
+      t.string :title
+      t.text :description
+      t.string :url
+      t.string :location
+      t.string :apply_link
+      t.timestamps
+    end
+  end
 end
