140ea9f2a2178b56dd2800241bdbe79f88e4cd2c
bodyyyya <vbogdanua177@gmail.com>
2024-02-03 21:05:18 +0200

    fixed tests

--- app/vacancy_pars.rb
+++ app/vacancy_pars.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'httparty'
 require 'dotenv/load'
@@ -20,30 +22,35 @@ class Scraper
   end
 
   def self.call
+    new.call
+  end
+
+  def call
     scraping
   end
 
-  def self.scraping
-      base_url = ENV['OPENAI_CAREERS_URL']
-      response = HTTParty.get(base_url)
-      handle_empty_response_body(response.body)
-      doc = Nokogiri::HTML(response.body)
-      vacancy_links = doc.css('ul[aria-label="All teams roles"] li a').map { |link| link['href'] if link['href'].include?("careers")}
-
-      vacancy_links.each do |link|
-        scrape_individual_vacancy("https://openai.com#{link}")
-      end
+  def scraping
+    base_url = ENV['OPENAI_CAREERS_URL']
+    response = HTTParty.get(base_url)
+    handle_empty_response_body(response.body)
+    doc = Nokogiri::HTML(response.body)
+    vacancy_links = doc.css('ul[aria-label="All teams roles"] li a').map do |link|
+      link['href'] if link['href'].include?('careers')
     end
-  private
 
-  def self.handle_empty_response_body(body)
-    if body.nil? || body.empty?
-      puts 'Error: Empty response body'
-      return
+    vacancy_links.each do |link|
+      scrape_individual_vacancy("https://openai.com#{link}")
     end
   end
 
-  def self.scrape_individual_vacancy(vacancy_url)
+  def handle_empty_response_body(body)
+    return unless body.nil? || body.empty?
+
+    puts 'Error: Empty response body'
+    nil
+  end
+
+  def scrape_individual_vacancy(vacancy_url)
     vacancy_response = HTTParty.get(vacancy_url)
     handle_empty_response_body(vacancy_response.body)
 
@@ -54,24 +61,23 @@ class Scraper
     location = vacancy_doc.css('.f-subhead-1').text.strip
     apply_link = vacancy_doc.at_css('.lg\\:absolute.top-0.left-0.right-0.flex.flex-col a[aria-label="Apply now"]')&.[]('href')
     save_vacancy(title, description, vacancy_url, location, apply_link)
-
   end
 
-  def self.save_vacancy(title, description, vacancy_url, location, apply_link)
+  def save_vacancy(title, description, vacancy_url, location, apply_link)
     existing_vacancy = Vacancy.find_by(url: vacancy_url)
-  
+
     if existing_vacancy
       update_vacancy(existing_vacancy, title, description, vacancy_url, location, apply_link)
     else
       create_vacancy(title, description, vacancy_url, location, apply_link)
     end
   end
-  
-  def self.update_vacancy(existing_vacancy, title, description, vacancy_url, location, apply_link)
+
+  def update_vacancy(existing_vacancy, title, description, _vacancy_url, location, apply_link)
     existing_vacancy.update(title: title, description: description, location: location, apply_link: apply_link)
   end
-  
-  def self.create_vacancy(title, description, vacancy_url, location, apply_link)
+
+  def create_vacancy(title, description, vacancy_url, location, apply_link)
     Vacancy.create(
       title: title,
       description: description,
--- spec/scraper_test.rb
+++ spec/scraper_test.rb
@@ -1,41 +1,59 @@
+# frozen_string_literal: true
+
 require 'rspec'
 require 'webmock/rspec'
-require_relative '../app/vacancy_pars.rb'  
+require_relative '../app/vacancy_pars'
 require_relative '../app/db'
 
 RSpec.describe Scraper do
-  describe '#call' do
+  describe '#scrape' do
     let(:base_url) { 'https://openai.com/careers/search' }
     let(:invalid_html) { '<html><head></head><body><p>Not a valid job listing page</p></body></html>' }
-    let(:saved_link) { 'https://openai.com/careers/account-associate' }
-    let(:scraper) { Scraper.new }
+    let(:saved_link) { 'https://openai.com/careers/analytics-data-engineer-applied-engineering' }
+    let(:scraper) {Scraper.new}
 
     before do
       stub_request(:get, ENV['OPENAI_CAREERS_URL'])
         .to_return(body: File.read('spec/vacancies_page.html'))
-      
+
       individual_vacancy_link = "#{ENV['OPENAI_CAREERS_URL']}/job1"
-    
+
       stub_request(:get, individual_vacancy_link)
         .to_return(body: File.read('spec/vacancies_page.html'))
     end
 
-    it 'scrapes and saves vacancies to the database' do
-      Database.connect_db
-      # Replace with the actual assertions based on your HTML content
-      vacancy = Vacancy.find_by(title: 'Account Associate')
-      expect(vacancy.title).to eq('Account Associate')
-      expect(vacancy.location).to eq('San Francisco, California, United States — Go To Market')
-      expect(vacancy.url).to eq(saved_link)
-    end
+    describe '#call' do
+      context 'when the request is successful' do
+        before { scraper.call }
 
+        it 'parses job data correctly' do
+          Database.connect_db
+          # Replace with the actual assertions based on your HTML content
+          vacancy = Vacancy.find_by(title: 'Analytics Data Engineer, Applied Engineering')
+          expect(vacancy.title).to eq('Analytics Data Engineer, Applied Engineering')
+          expect(vacancy.location).to eq('San Francisco, California, United States — Applied AI Engineering')
+          expect(vacancy.url).to eq(saved_link)
+        end
+      end
+
+      context 'when the request fails' do
+        before do
+          stub_request(:get, base_url).to_return(status: 500)
+          stub_request(:get, saved_link).to_return(status: 500)
+        end
+
+        it 'handles the error gracefully' do
+          expect { scraper.call }.not_to raise_error
+        end
+      end
+    end
     context 'when the HTML structure is unexpected' do
       before do
         stub_request(:get, base_url).to_return(status: 200, body: invalid_html)
       end
 
       it 'handles the parsing error gracefully' do
-        expect { scraper.scraping }.not_to raise_error
+        expect { scraper.call }.not_to raise_error
       end
     end
   end
